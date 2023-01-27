f8492487a4ba92ce528d4d74bdae0951b17487d0
mimus-assa <73607556+mimus-assa@users.noreply.github.com>
2023-01-27 01:16:02 -0600

    varios cambios
    agrege  funcionalidades para el onehot y se crearon varios nuevos metodos como el oversampling

--- utils.py
+++ utils.py
@@ -1,6 +1,20 @@
+import pandas as pd
+import numpy as np
+import joblib
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+import tensorflow_addons as tfa
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+import transformers
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from typing import Tuple, List
+import tensorflow.keras.backend as K
+
+
+
 class Preprocesing:
-    '''esta clase contiene las herramientas para crear 
-    el dataset a partir de las configuraciones'''
     
     def __init__(self, conf):
         self.batch_size=conf["batch_size"]
@@ -12,15 +26,10 @@ class Preprocesing:
         self.file_1=conf["file_1"]
         self.scaler_ohlc=conf["scaler_ohlc"]
         self.cols = conf["cols"]
-         
-    def load_data(self, path: str, cols: List[str]) -> pd.DataFrame:
-        data = pd.read_csv(path, index_col='timestamp')
-        mask = (data.index >= self.start_timestamp) & (data.index < self.end_timestamp)
-        data = data[mask][cols]
-        return data
+        self.one_hot=conf["one_hot"]
+        self.nan_rows = []
 
-
-    def transform_data(self, data: pd.DataFrame, cols_stan: List[str], cols_norm: List[str], scaler_filename: str) -> pd.DataFrame:
+    def scale_and_save(self, data: pd.DataFrame, cols_stan: List[str], cols_norm: List[str], scaler_filename: str):
         if cols_stan:
             scaler_stan = StandardScaler()
             data[cols_stan] = scaler_stan.fit_transform(data[cols_stan])
@@ -28,9 +37,59 @@ class Preprocesing:
         if cols_norm:
             scaler_norm = MinMaxScaler(feature_range=(0, 1))
             data[cols_norm] = scaler_norm.fit_transform(data[cols_norm])
-            joblib.dump(scaler_norm, scaler_filename+'_norm') 
+            joblib.dump(scaler_norm, scaler_filename+'_norm')
         return data
 
+
+    def update_new_position(self, df, current_index, value):
+        for i in range(1, 24):
+            if df.loc[current_index-i, 'position'] != -value:
+                df.loc[current_index-i, 'new_position'] = value
+            else:
+                break
+
+
+    def load_data(self, path: str, cols: List[str]) -> pd.DataFrame:
+        data = pd.read_csv(path, index_col='timestamp')
+        data["timestamp"]=data.index
+        col1_counts = data['position'].value_counts()
+        col1_percent = col1_counts / data.shape[0] * 100
+        print(col1_percent)
+        mask = (data.index >= self.start_timestamp) & (data.index < self.end_timestamp)
+        data = data[mask][cols]
+        if not self.one_hot:
+            data = data[data.position != 0]
+            data.loc[data.position == -1, :] = 0
+        return data
+
+    def data_oversample(self,df):
+        ts = df.index
+        df=df.reset_index(drop=True)
+        df['new_position'] = df['position']
+        for i in range(len(df)):
+            if df.loc[i, 'position'] == 1:
+                self.update_new_position(df, i, 1)
+            elif df.loc[i, 'position'] == -1:
+                self.update_new_position(df, i, -1)
+        df=df.drop(["position"],axis=1)
+        df = df.rename(columns={'new_position': 'position'})#we should print the oversample percentages
+        df.index=ts
+        df = df.iloc[1:]
+        col1_counts = df['position'].value_counts()
+        col1_percent = col1_counts / df.shape[0] * 100
+        print(col1_percent)
+        return df
+
+
+    def transform_data(self, data: pd.DataFrame, cols_stan: List[str], cols_norm: List[str], scaler_filename: str) -> pd.DataFrame:
+        self.nan_rows = data[data.isna().any(axis=1)]
+        print(len(self.nan_rows))
+        if not self.one_hot:
+            data = self.scale_and_save(data, cols_stan, cols_norm, scaler_filename)
+        else:
+            data = pd.get_dummies(data, columns=['position'])
+            data = self.scale_and_save(data, cols_stan, cols_norm, scaler_filename)
+        return data
 
     def data_splitting(self, data: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray, np.ndarray, pd.DataFrame, pd.DataFrame, pd.DataFrame]:
         times = data.index.values
@@ -42,69 +101,46 @@ class Preprocesing:
         train_num = data[data.index < last_20pct]
         val_num = data[(data.index >= last_20pct_plus_seqlen) & (data.index < last_10pct)]
         test_num = data[data.index >= last_10pct_plus_seqlen]
-        
+
         train_num_df = train_num.copy()
         val_num_df = val_num.copy()
         test_num_df = test_num.copy()
-        
+
         # Remove print statements
         self.ts_forcallback = test_num_df.index[0]
-        
+
         # Convert pandas columns into arrays
         train_num = train_num.values
         val_num = val_num.values
         test_num = test_num.values
-        return train_num, val_num, test_num, train_num_df, val_num_df, test_num_df
+        if self.one_hot:
+            train_num_labels = train_num[:, -4:]
+            val_num_labels = val_num[:, -4:]
+            test_num_labels = test_num[:, -4:]
+            return train_num, val_num, test_num, train_num_labels, val_num_labels, test_num_labels, train_num_df, val_num_df, test_num_df
+        else:
+            return train_num, val_num, test_num, train_num_df, val_num_df, test_num_df
 
-    def sequence_creator(self, datas):
-        '''cambiamos cada entrada por una secuencia de 
-        las ultimas seq_len filas y despues incluimos 
-        las salidas con indice seq_len+1 '''
-        data1=pd.DataFrame(datas[0])
-        X_0, y1= [],[]
-        for i in range(self.seq_len, len(data1)):
-            X_0.append(data1.reset_index(drop=True).iloc[i-self.seq_len:i])
-            y1.append(data1.iloc[:, -1].reset_index(drop=True).iloc[i])
-        X_0   = np.array(X_0)
-        y1  = np.array(y1).reshape((-1,1))
+    def sequence_creator(self, data):
+        X_0, y1 = [], []
+        for i in range(self.seq_len, len(data)):
+            X_0.append(data[i-self.seq_len:i])
+            if not self.one_hot:
+                y1.append(data[i, -1])
+            else:
+                labels = data[i, -4:]
+                y1.append(labels)
+        X_0 = np.array(X_0)
+        y1 = np.array(y1)
         return X_0, y1
-  
+
     def dataset_gen(self, df, targets):
         """Generates dataset (X, Y) for training"""
+        
         x_0 = tf.constant([x for x in df["x_0"].values])
         X = {"input_0": x_0}
-        Y = {"dense_1": targets}
+        Y = {"output_1": targets}
         train_dataset = tf.data.Dataset.from_tensor_slices((X, Y))
         train_dataset = train_dataset.batch(self.batch_size)
         return train_dataset
-
-    def dataset_exploration(self, train_data, val_data, test_data):
-        '''Function to explore the datasets generated by the create_dataset function'''
-        train_size = 0
-        val_size = 0
-        test_size = 0
-        for data in train_data:
-            train_size += data[0]['input_0'].numpy().nbytes
-            shape = data[0]['input_0'].numpy().shape
-            dtypes = data[0]['input_0'].dtype
-            target_shape = data[1]['dense_1'].numpy().shape
-            target_dtypes = data[1]['dense_1'].dtype
-            mean = tf.reduce_mean(data[0]['input_0']).numpy()
-            std = tf.math.reduce_std(data[0]['input_0']).numpy()
-            min_val = tf.math.reduce_min(data[0]['input_0']).numpy()
-            max_val = tf.math.reduce_max(data[0]['input_0']).numpy()
-        for data in val_data:
-            val_size += data[0]['input_0'].numpy().nbytes
-        for data in test_data:
-            test_size += data[0]['input_0'].numpy().nbytes
-        total_mem = train_size + val_size + test_size
-        print(f"Total memory usage: {total_mem / (1024 ** 2):.2f}MB")
-        print(f"Train data shape: {shape}")
-        print(f"Validation data shape: {shape}")
-        print(f"Test data shape: {shape}")
-        print(f"Data types: {dtypes}")
-        print(f"Target shape: {target_shape}")
-        print(f"Target dtypes: {target_dtypes}")
-        print(f"Data range: Min: {min_val}, Max: {max_val}")
-        print(f"Data mean: {mean}")
-        print(f"Data standard deviation: {std}")
+        
